9dc44ba97f80cef9e3753c597bd68d8afd6024df
GuilhermeTurolla <guilherme_turolla@hotmail.com>
2021-05-18 17:34:29 -0300

    Commit 4

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(() => {
     $("#adicionar").click(() =>{
-        console.log("teste3")
+        console.log("teste4")
         $("#table-dados tbody").append(
         `<tr>
             <td>#</td>   
