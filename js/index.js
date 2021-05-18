5433897d74e7fbd1581833afa8c7eb6a18e683cf
GuilhermeTurolla <guilherme_turolla@hotmail.com>
2021-05-18 17:21:46 -0300

    Commit teste 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(() => {
     $("#adicionar").click(() =>{
-        console.log("teste")
+        console.log("teste2")
         $("#table-dados tbody").append(
         `<tr>
             <td>#</td>   
